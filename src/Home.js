2a745cac50093aeed5d9e3ca2dcb313489ea19d9
LunarTear09 <fs.de500@gmail.com>
2024-04-15 03:09:20 +0300

    page

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Totalstats() {
   // State example
- 
+  const [count, setCount] = useState(0);
 
   // Effect example
   useEffect(() => {
@@ -10,13 +10,11 @@ function Totalstats() {
     document.title = `You clicked ${count} times`;
   }, [count]);
 
-
-
   return (
     <div>
       <h1>My React Component</h1>
       <p>You clicked {count} times</p>
-      
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
