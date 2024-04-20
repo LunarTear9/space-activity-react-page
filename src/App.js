140a5ce5c11800965c4b51673100d04a5fd4f1b1
LunarTear09 <fs.de500@gmail.com>
2024-04-21 00:46:28 +0300

    Solar Energetic Particle Patch

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       solarFlares: [], // Store solar flare data
+      solarParticles: [], // Store solar particle data
+      isLoading: true, // Loading state
     };
   }
 
@@ -17,21 +19,47 @@ class App extends React.Component {
     // Fetch solar flare data from the NASA API
     fetch('https://api.nasa.gov/DONKI/FLR?startDate=2024-02-01&endDate=2024-03-01&api_key=MWW5tddeORnLlIE9KVNucSorRdAYtwYayolRlmI7')
       .then(response => response.json())
-      .then(data => this.setState({ solarFlares: data }))
-      .catch(error => console.error('Error fetching solar flare data:', error));
+      .then(data => this.setState({ solarFlares: data, isLoading: false })) // Update loading state when data is fetched
+      .catch(error => {
+        console.error('Error fetching solar flare data:', error);
+        this.setState({ isLoading: false }); // Update loading state in case of an error
+      });
+
+    // Fetch solar particle data from the NASA API
+    fetch('https://api.nasa.gov/DONKI/SEP?startDate=2024-02-01&endDate=2024-03-01&api_key=MWW5tddeORnLlIE9KVNucSorRdAYtwYayolRlmI7')
+      .then(response => response.json())
+      .then(data => this.setState({ solarParticles: data })) // Update loading state when data is fetched
+      .catch(error => {
+        console.error('Error fetching solar particle data:', error);
+        this.setState({ isLoading: false }); // Update loading state in case of an error
+      });
   }
 
   render() {
-    const { solarFlares } = this.state;
+    const { solarFlares, solarParticles, isLoading } = this.state;
     const imageUrl = 'https://cdn.mos.cms.futurecdn.net/GY9JBbrzsGsz2psUv6pgdV.jpg';
+
+    if (isLoading) {
+      return (
+        <div className="loading-container">
+          <div className="Grid2">
+          <img src={iconLogo} alt="Icon" className="icon" style={{ width: '165px', height: '160px',marginBottom: 120 }} />
+          <div className="loader"></div>
+          </div>
+        </div>
+      ); // Loading spinner
+    }
+
     return (
       <Router>
         <div className='App'>
           <NavBar />
-          <div className="background"
+          <div
+            className="background"
             style={{
               backgroundImage: `url(${imageUrl})`,
-            }}>
+            }}
+          >
             <div className="content">
               <Routes>
                 <Route path="/stats" element={<Totalstats />} />
@@ -40,7 +68,7 @@ class App extends React.Component {
             <div className='MainContainer'>
               <div className='Container4'>
                 <div className='Row'>
-                  <img src={iconLogo} alt="Icon" className="icon" style={{ width: '65px', height: '60px', marginTop: 30, marginLeft: 60 }} />
+                <img src={iconLogo} alt="Icon" className="icon" style={{ width: '65px', height: '60px', marginTop: 30, marginLeft: 60 }} />
                   <h1 style={{ marginLeft: 40, marginTop: 35 }}>Live Space Data Project</h1>
                 </div>
                 {/* Display solar flare data */}
@@ -56,6 +84,26 @@ class App extends React.Component {
                         <p><a href={flare.link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: 'black' }}>More Info</a></p>
                       </div>
                     ))}
+                  </div>
+                  <h1 style={{ marginLeft: 40 }}>Solar Energetic Particle</h1>
+                  <div className='Grid'>
+                    {solarParticles.map(particle => (
+                      <div key={particle.sepID} className='mini-container'>
+                        <h3>{particle.instruments[0].displayName}</h3>
+                        <p><strong>Event Time:</strong> {particle.eventTime}</p>
+                        <p><a href={particle.link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: 'black' }}>More Info</a></p>
+                      </div>
+                    ))}
+                    {/* Render empty containers for the difference */}
+                    {solarFlares.length > solarParticles.length &&
+                      Array(solarFlares.length - solarParticles.length).fill().map((_, index) => (
+                        <div key={index} className='mini-container empty'>
+                          <h3>&nbsp;</h3>
+                          <p><strong>Event Time:</strong> </p>
+                          <p><a href="#" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: 'black' }}>&nbsp;</a></p>
+                        </div>
+                      ))
+                    }
                   </div>
                 </div>
               </div>
--- src/navigation.js
+++ src/navigation.js
@@ -1,46 +1,31 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css';
 import iconLogo from './assets/mylogo.png';
 
 const handleIconClick2 = () => {
-    window.location.href = "https://www.pitmtech.com";
-  };
-
+  window.location.href = "https://www.pitmtech.com";
+};
 
 const NavBar = () => {
-return (
+  return (
     <nav className="navbar">
-        <ul className="nav-list">
-
-       
-
-<div className = "links">
-    <div className="Container">
-<div className="icon-container" onClick={handleIconClick2}>
-            <img src={iconLogo} alt="Icon" className="icon" style={{ width: '110px', height: '100px', cursor: 'pointer' }} />
+      <ul className="nav-list">
+        <div className="links">
+          <div className="Container">
+            <div className="icon-container" onClick={handleIconClick2}>
+              <img src={iconLogo} alt="Icon" className="icon" style={{ width: '110px', height: '100px', cursor: 'pointer' }} />
+            </div>
+            <div className="HeaderList">
+            <li><a href="https://weather.pitmtech.com">Weather Page</a></li> {/* Added link */}
+              <li><Link to="/" >Home</Link></li>
+              
+            </div>
           </div>
-          <div className="HeaderList">
-            <li><Link to="/" >Home</Link></li>
-          
-        
-        
         </div>
-          </div>
-</div>
-       
       </ul>
-
-</nav>
-);
-
-
-
-
-//<Link to="/">App</Link>
-//<Link to="/AppB">AppB</Link>
-
-
-
+    </nav>
+  );
 }
 
 export default NavBar;
